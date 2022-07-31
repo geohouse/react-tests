6fef58f14f452f06333239481ccb42a47d0a04be
Geoffrey House <geo.house7@gmail.com>
2022-07-30 21:15:30 -0600

    fully working tic tac toe example

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -2,32 +2,59 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-class Square extends React.Component {
-  // Removed because Square no longer is keeping track of the state of the game (Board is instead)
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //       value: null,
-  //     };
-  //   }
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick({ value: "X" })}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
+
+// class Square extends React.Component {
+//   // Removed because Square no longer is keeping track of the state of the game (Board is instead)
+//   //   constructor(props) {
+//   //     super(props);
+//   //     this.state = {
+//   //       value: null,
+//   //     };
+//   //   }
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => this.props.onClick({ value: "X" })}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
+  }
+
+  // This method keeps track of the state changes for all Square components
+  // (they call this function when their state changes)
+  handleClick(i) {
+    //const squares = this.state.squares.slice();
+    // Copy array before modifying it.
+    const squares = [...this.state.squares];
+    //Ignore a click if someone has already won, or the square is already filled
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    //squares[i] = "X";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   renderSquare(i) {
     return (
@@ -39,7 +66,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    //const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    }
 
     return (
       <div>
@@ -81,6 +115,27 @@ class Game extends React.Component {
 }
 
 // ========================================
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[c] && squares[a] === squares[b]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
